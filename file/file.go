f9776077f832f685285e08d4570f78baa8e04638
purfree <purfree2703@gmail.com>
2018-12-13 17:43:00 +0800

    nothing

--- file/file.go
+++ file/file.go
@@ -3,6 +3,7 @@ package file
 import (
 	"bytes"
 	"os"
+	"unicode/utf8"
 )
 
 // 判断所给路径文件/文件夹是否存在
@@ -31,30 +32,23 @@ func IsFile(path string) bool {
 	return !IsDir(path)
 }
 
-// 预设每列数据的长度
+// 输出的内容排版，使每列数据宽度一致。（不完全准确）
 func FillBlank(l ...int) func(cells ...string) string {
 	return func(cells ...string) string {
 		var bb bytes.Buffer
 		for i, cell := range cells {
-			var b []byte
-			if i+1 > len(l) {
-				b = make([]byte, len(cell))
-			} else {
-				b = make([]byte, l[i])
-			}
-			if len(cell) > len(b) {
-				// 预设宽度不够，重新设置
-				b = make([]byte, len(cell))
-				l[i] = len(cell)
-			}
-			for j, _ := range b {
-				if j+1 > len(cell) {
-					b[j] = ' '
+			bb.WriteString(cell)
+			var sum int
+			for _, r := range cell {
+				if utf8.RuneLen(r) > 2 {
+					sum += 2
 				} else {
-					b[j] = cell[j]
+					sum += 1
 				}
 			}
-			bb.Write(b)
+			for ; sum < l[i]; sum++ {
+				bb.WriteRune(' ')
+			}
 		}
 		return bb.String()
 	}
